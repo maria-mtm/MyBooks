4ac0f37031d7803014f2c85ca78ae27d21a0b7b5
melendezcabreram <melendezcabreram@gmail.com>
2023-02-24 19:32:53 +0100

    working on

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { FormularioRegistroComponent } from './component/formulario-registro/formulario-registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormularioRegistroComponent
   ],
   imports: [
     BrowserModule,
