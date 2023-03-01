6f97e269c4e2f74c48257e7e17689057cd5137ff
melendezcabreram <melendezcabreram@gmail.com>
2023-03-01 20:32:03 +0100

    in process

--- src/app/models/libro.ts
+++ src/app/models/libro.ts
@@ -1,18 +1,9 @@
 export class Libro {
-
-    public id_libro: number
-    public id_usuario: number
-    public titulo: string
-    public autor: string
-    public precio: number
-    public photo: string
-
-    constructor(id_libro: number, id_usuario: number, titulo: string, autor: string, precio: number, photo: string) {
-        this.id_libro = id_libro;
-        this.id_usuario = id_usuario;
-        this.titulo = titulo;
-        this.autor = autor;
-        this.precio = precio;
-        this.photo = photo;
-    }
+    constructor(
+        public id_libro: number,
+        public id_usuario: number,
+        public titulo: string,
+        public autor: string,
+        public precio: number,
+        public photo: string) {}
 }
--- src/app/pages/libros/libros.component.ts
+++ src/app/pages/libros/libros.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Libro } from 'src/app/models/libro';
 
 @Component({
   selector: 'app-libros',
   templateUrl: './libros.component.html',
   styleUrls: ['./libros.component.css']
 })
-export class LibrosComponent {
+export class LibrosComponent implements OnInit{
+  public libros: Libro[];
+  constructor()
+  {
 
+    // this.libros=[]
+    this.libros=[
+      new Libro(1,2,"Casita azul", "Carlos", 25, "photo_casitaazul"),
+      new Libro(2,4,"Mis memorias", "Inma", 28, "photo_mismemorias")
+    ]
+  }
+  ngOnInit(): void {}
+  
 }
