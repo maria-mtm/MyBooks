6a6e7626e581ab1db925ef887848cbb8457dc363
melendezcabreram <melendezcabreram@gmail.com>
2023-03-03 18:25:57 +0100

    casi

--- src/app/pages/libros/libros.component.ts
+++ src/app/pages/libros/libros.component.ts
@@ -11,11 +11,21 @@ export class LibrosComponent implements OnInit{
   constructor()
   {
 
-    this.libros=[]
-    // this.libros=[
-    //   new Libro(1,2,"Casita azul", "Carlos", 25, "photo_casitaazul"),
-    //   new Libro(2,4,"Mis memorias", "Inma", 28, "photo_mismemorias")
-    // ]
+    // this.libros=[]
+    this.libros=[
+      new Libro(1,2,"Casita azul", "Carlos", 25, "photo_casitaazul"),
+      new Libro(2,4,"Mis memorias", "Inma", 28, "photo_mismemorias")
+    ]
+  }
+  public add(inputId_libro:HTMLInputElement, inputId_usuario:HTMLInputElement,inputTitulo:HTMLInputElement,inputAutor:HTMLInputElement,inputPrecio:HTMLInputElement, inputPhoto:HTMLInputElement){
+    
+    this.libros[0].id_libro=inputId_libro.valueAsNumber;
+    this.libros[0].id_usuario=inputId_usuario.valueAsNumber;
+    this.libros[0].titulo=inputTitulo.value;
+    this.libros[0].autor=inputAutor.value;
+    this.libros[0].precio=inputPrecio.valueAsNumber;
+    this.libros[0].photo=inputPhoto.value;
+   
   }
   ngOnInit(): void {}
   
