1f3b0d697b40a44f11ac1682177ce8527429d972
melendezcabreram <melendezcabreram@gmail.com>
2023-03-06 21:30:30 +0100

    in progress

--- src/app/pages/libros/libros.component.ts
+++ src/app/pages/libros/libros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Libro } from 'src/app/models/libro';
+import { ServicioService } from 'src/app/shared/servicio.service';
 
 @Component({
   selector: 'app-libros',
@@ -8,17 +9,22 @@ import { Libro } from 'src/app/models/libro';
 })
 export class LibrosComponent implements OnInit{
   public libros: Libro[];
-  constructor()
-  {
 
-    this.libros=[]
-    this.libros=[
-      new Libro(1,2,"Casita azul", "Carlos", 25, "https://imagenes.elpais.com/resizer/sMWeM3Z0awXbSkNaTMExLR_QNLU=/1200x0/arc-anglerfish-eu-central-1-prod-prisa.s3.amazonaws.com/public/YC5XJK5X2DES4MGR2W3HWWS7JU.jpg"),
-      new Libro(2,4,"Mis memorias", "Inma", 28, "photo_mismemorias")
-    ]
+  constructor(public ServicioService: ServicioService)
+  {
+//estoy igualando la nueva variable libros al libros que hay en servicioService que ya contiene el array de Libros
+  // this.libros=this.ServicioService.libros
+  this.libros=this.ServicioService.getAll()
+  
   }
+
+  // add(id_libro:number, id_usuario:number,titulo:string, autor:string, precio: number, photo:string ){
+  //   this.ServicioService.libros.push(new Libro(id_libro, id_usuario, titulo, autor, precio, photo));
+
+  //   console.log(this.ServicioService.libros)
+  // }
   public add(inputId_libro:HTMLInputElement, inputId_usuario:HTMLInputElement,inputTitulo:HTMLInputElement,inputAutor:HTMLInputElement,inputPrecio:HTMLInputElement, inputPhoto:HTMLInputElement){
-    this.libros.push(new Libro(
+    this.ServicioService.libros.push(new Libro(
     inputId_libro.valueAsNumber,
     inputId_usuario.valueAsNumber,
     inputTitulo.value,
@@ -26,7 +32,13 @@ export class LibrosComponent implements OnInit{
     inputPrecio.valueAsNumber,
     inputPhoto.value
     ))
+    console.log(this.ServicioService.libros)
   }
+
+  public getOne(inputIds_libro:HTMLInputElement){
+    this.ServicioService.getOne(inputIds_libro.valueAsNumber)
+  }
+
   ngOnInit(): void {}
   
 }
